7a8f6489b53ea47fc443eff504b2fa9e9533e680
nirReich <reich.nir@gmail.com>
2025-01-28 11:23:29 +0200

    share page added

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,11 +1,37 @@
 import React from "react";
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
-const MealPage = async({params}) => {
+const MealPage = async ({ params }) => {
+  const meal = getMeal(params.mealSlug);
+  if (!meal) {
+    notFound()
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
   return (
-    <div>
-      <div>Meal comp</div>
-     <div>{await params.mealSlug}</div>
-    </div>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image alt={meal.title} fill src={meal.image} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
   );
 };
 
